1cf671a3b2ddcb45f798fab630cd68671eebc56e
Barkir <r.barinov2006@gmail.com>
2025-11-25 23:13:37 +0300

    Added VAO, EBO

---
+++ OpenGL/include/VAO.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GL/glew.h>
+#include "VBO.hpp"
+
+class VAO {
+    public:
+        GLuint ID;
+        VAO();
+
+        void LinkVBO(VBO VBO, GLuint layout);
+        void Bind();
+        void Unbind();
+        void Delete();
+};
--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,4 +1,4 @@
-#define GLEW_STATIC
+// #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
@@ -6,6 +6,12 @@
 #include <string>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
+#include "shader.hpp"
+#include "VAO.hpp"
+#include "VBO.hpp"
+#include "EBO.hpp"
+
 
 // #include "camera.hpp"
 
@@ -49,39 +55,15 @@ int main(void) {
         0.0f,  0.5f, 0.0f  // Top
     };
 
+    Shader shaderProgram("../shaders/vertex.vert", "../shaders/fragment.frag");
+    VAO VAO1;
+    VAO1.Bind();
 
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
+    VBO VBO1(vertices, sizeof(vertices));
 
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // wrapping programs into shader programs
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    GLuint VAO, VBO;
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);       // GL_STREAM - modified once and used once
-                                                                                     // GL_STATIC - modified once and used many times
-                                                                                     // GL_DYNAMIC - modified multiple times and used many times
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    VAO1.LinkVBO(VBO1, 0);
+    VAO1.Unbind();
+    VBO1.Unbind();
 
     while (!glfwWindowShouldClose(window)) {
 
@@ -89,15 +71,16 @@ int main(void) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        shaderProgram.Activate();
+        VAO1.Bind();
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
+    VAO1.Delete();
+    VBO1.Delete();
+    shaderProgram.Delete();
 
     glfwDestroyWindow(window);
     glfwTerminate();
